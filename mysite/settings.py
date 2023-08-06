fe45b91fe8e8ce900e90259664078ffa89515438
Faith Burnett <fbrnttt@gmail.com>
2023-08-05 20:52:35 -0500

    debug change

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-08@0)il$--iazxlkhg5nq$esv&##ow)qt)z*hz87oi($&uv7#*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https//www.pythonanywhere.com"]
 
 
 # Application definition
